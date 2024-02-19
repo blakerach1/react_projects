37fc0f739cb109c70a834894237befc2977309e2
Rachel Blake <blakerach1@gmail.com>
2024-02-19 17:58:20 +0800

    react router tutorial

--- react-router-tutorial/src/main.jsx
+++ react-router-tutorial/src/main.jsx
@@ -1,20 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// Here we import our pages
+import HomePage from "./pages/HomePage.jsx";
+import AboutPage from "./pages/AboutPage.jsx";
+import ContactPage from "./pages/ContactPage.jsx";
+
+// Here we import our components
+import NavBar from "./components/NavBar.jsx";
+
+// Here we create our router and tell it what pages to render at what path
+
+const router = createBrowserRouter([
+  {
+    path: "/", // This is the root path
+    // putting our NavBar as the main component will cause the children to render in the <Outlet />
+    element: <NavBar />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/about", element: <AboutPage /> },
+      { path: "/contact", element: <ContactPage /> },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* Here we wrap our app in the router provider so the pages render */}
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// This file can be considered the domain file of our React application.
-// It is the entry point for the application and is responsible for
-// rendering the root component of the application into the DOM.
-// This is the library or part of the React library that talks to the HTML
-// we're telling it to create a new element at document.getElementById('root')
-// and then render the App component into that element, so that there is a link between React and HTML
-
-// The ReactDOM.createRoot method is used to create a new root for a React tree.
-// rendering an app component into the root element
